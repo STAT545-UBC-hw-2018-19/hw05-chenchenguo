5b97d95e152a575801757f8b77efe5011d7a44a2
Chenchen Guo <756775098@qq.com>
2018-10-17 15:28:56 -0700

    last

---
+++ Life_arrange.R
@@ -0,0 +1,5 @@
+structure(list(continent = structure(c(5L, 4L, 1L, 2L, 3L), .Label = c("Africa", 
+"Americas", "Asia", "Europe", "Oceania"), class = "factor"), 
+    variance = c(14.4066633025362, 29.5194210961854, 83.7263472327345, 
+    87.3306697799219, 140.767108377472)), class = c("tbl_df", 
+"tbl", "data.frame"), row.names = c(NA, -5L))
